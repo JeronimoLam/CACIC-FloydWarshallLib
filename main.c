be3430c330d496532894a6a4c964d5d58b330ef9
Jeronimo <jerolambre@gmail.com>
2024-03-21 01:04:09 -0300

    added .exe files

--- main.c
+++ main.c
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
 
     FW_Matrix data = create_structure(dataType, path, -1);
     compute_FW(data);
-    save_structure(data, "./output/", "Result.csv", CSV, 1, 1);
+    save_structure(data, "./output/", "Result.csv", CSV, 1, 0);
 
     // Closes the file
     fclose(file);
