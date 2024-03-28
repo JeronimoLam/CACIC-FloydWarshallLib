0667fe18f64516f1aa77ff50c0917ea6dcd56e31
Jeronimo <jerolambre@gmail.com>
2024-03-28 18:21:25 -0300

    Refactor

--- Floyd_Warshall_Lib/File_Manager/CSV/CSV_createMatrix.c
+++ Floyd_Warshall_Lib/File_Manager/CSV/CSV_createMatrix.c
@@ -25,55 +25,6 @@ void *CSV_createMatrix(FW_Matrix FW, FILE *file)
     }
 }
 
-// These functions are similar but each one uses atoi, atof, strtod, or a simple assignment, respectively.
-// static void *createIntMatrix(FILE *file, unsigned int size, unsigned int BS) {
-//     rewind(file);
-
-//     int *matrix = (int *)malloc(size * size * sizeof(int));
-//     if (!matrix) exit(9); // Allocation failed
-//     char line[12040];
-//     char *token;
-//     int row = 0, col = 0;
-
-//     // Initialize while reading the file
-//     for (row = 0; row < size; ++row) {
-//         col = 0; // Reset column counter for each row
-//         int readPartialLine = 0; // Flag to indicate if we have a partial line
-
-//         do {
-//             if (fgets(line, sizeof(line), file)) {
-//                 // Check if fgets read the entire line or just a part of it
-//                 char* newlinePos = strchr(line, '\n');
-//                 if (newlinePos != NULL) {
-//                     *newlinePos = '\0'; // Terminate the string at the newline character, effectively removing it
-//                     readPartialLine = 0; // We've read the full line
-//                 } else if (!feof(file)) {
-//                     readPartialLine = 1; // The line was too long and got cut off, so we have a partial line
-//                 }
-
-//                 token = strtok(line, ",");
-//                 while (token != NULL && col < size) {
-//                     char *trimmedToken = trim(token);
-//                     if (strcmp(trimmedToken, "INF") == 0 || atoi(trimmedToken) == -1) {
-//                         matrix[row * size + col] = INT_MAX;
-//                     } else {
-//                         matrix[row * size + col] = atoi(trimmedToken);
-//                     }
-//                     token = strtok(NULL, ",");
-//                     col++;
-//                 }
-//             }
-//         } while (readPartialLine); // Continue if we have a partial line
-
-//         // Fill remaining columns in the current row with INT_MAX
-//         for (; col < size; ++col) {
-//             matrix[row * size + col] = INT_MAX;
-//         }
-//     }
-
-//     return reorganizeToBlocks(matrix, size, BS, TYPE_INT);
-// }
-
 void* createIntMatrix(FILE* file, unsigned int size, unsigned int norm_size, int BS) {
     rewind(file); // Ensure we start reading from the beginning of the file
     
@@ -105,9 +56,8 @@ void* createIntMatrix(FILE* file, unsigned int size, unsigned int norm_size, int
         }
     }
 
-
-    return reorganizeToBlocks(matrix, norm_size, BS, TYPE_INT);
-     
+    int * matrix2 = reorganizeToBlocks(matrix, norm_size, BS, TYPE_INT);
+    return matrix2;
 }
 
 static void *createFloatMatrix(FILE *file, unsigned int size, unsigned int BS)
